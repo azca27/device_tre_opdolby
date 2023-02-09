itsshashanksp/device_xiaomi_sm6150-common

sm6150-common: import KProfile apk packages * from https://github.com/CannedShroud/android_packages_apps_KProfiles
Signed-off-by: shashank patil <9945shashank@gmail.com>

KProfiles/src/com/android/kprofiles/battery/KprofilesSettingsActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2022 CannedShroud
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.kprofiles.battery;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.collapsingtoolbar.R;
+
+public class KprofilesSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_KPROFILES = "kprofiles";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new KprofilesSettingsFragment(), TAG_KPROFILES).commit();
+    }
+}

azca27/device_tre_opdolby

Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

KProfiles/src/com/android/kprofiles/battery/KprofilesSettingsActivity.java
@@ -1,0 +1,34 @@
+/*
+ * Copyright (C) 2022 CannedShroud
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.kprofiles.battery;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.widget.R;
+
+public class KprofilesSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_KPROFILES = "kprofiles";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new KprofilesSettingsFragment(), TAG_KPROFILES).commit();
+    }
+}